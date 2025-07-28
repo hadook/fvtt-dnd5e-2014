5e37978401dd65387d8f3fbf226c4be17c47eeb4
hadook <hadook13@gmail.com>
2025-07-29 01:04:06 +0200

    Added script to toggle hotbar UI

---
+++ scripts/hotbar.js
@@ -0,0 +1,21 @@
+Hooks.once("init", () => {
+    game.settings.register("hadook-dnd5e-2014", "hideHotbar", {
+        name: "Hide Hotbar",
+        hint: "Hides the hotbar UI. The hotbar remains functional when hidden, macros can still be executed via keybinds.",
+        scope: "client",
+        config: true,
+        type: Boolean,
+        default: false,
+        onChange: value => {
+            const hotbarElement = document.getElementById("hotbar");
+            if (hotbarElement) hotbarElement.style.display = value ? "none" : "";
+        },
+        requiresReload: false,
+    });
+});
+
+Hooks.once("ready", async () => {
+    const hidden = game.settings.get("hadook-dnd5e-2014", "hideHotbar");
+    const hotbarElement = document.getElementById("hotbar");
+    if (hotbarElement) hotbarElement.style.display = hidden ? "none" : "";
+});
